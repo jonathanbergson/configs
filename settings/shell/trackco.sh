ee45571984c546a02ce0d5ccd98bce8ccd7fe82b
Jonathan Bergson <contato@bergson.me>
2021-12-23 08:19:04 -0300

    chore: novos comandos da trackco

--- settings/shell/trackco.sh
+++ settings/shell/trackco.sh
@@ -1,6 +1,11 @@
-cdb() cd ~/www/track/a2/backend/cmd/core;
-cdf() cd ~/www/track/a2/frontend/track;
-cdd() cd ~/www/track/design-system;
-cdui() cd ~/www/track-ui;
+cdb() cd ~/www/github/trackco/a2/backend/cmd/core;
+cdf() cd ~/www/github/trackco/a2/frontend/track;
+cdd() cd ~/www/github/trackco/design-system;
+cdd() cd ~/www/github/trackco/api-credits;
+
+trackco:code() code ~/www/github/trackco/a2
+trackco:code:front() code ~/www/github/trackco/a2/frontend/track;
+
+cdui() cd ~/www/github/jonathanbergson/track-ui;
 
 # runb() cd ~/www/a2/backend/cmd/core && go run main.go;
