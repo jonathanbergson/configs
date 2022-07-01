3ccd9f5d8af94060f5c1d71dde4fee64e2bad0f9
Jonathan Bergson <contato@bergson.me>
2022-07-01 15:57:54 -0300

    chore: novos comandas shell

--- settings/shell/trackco.sh
+++ settings/shell/trackco.sh
@@ -1,14 +1,39 @@
-track:ds() cd ~/www/github/trackco/design-system;
-code:track:ds() code ~/www/github/trackco/design-system;
+cd:a2() {
+  cd ~/www/github/trackco/a2 && pwd;
+}
+code:a2() {
+  code ~/www/github/trackco/a2;
+}
 
-cxm:a2() cd ~/www/github/trackco/a2
-code:cxm:a2() code ~/www/github/trackco/a2
+cd:back() {
+  cd ~/www/github/trackco/a2/backend/cmd/core && pwd;
+}
+run:back() {
+  clear && cd:back && go run main.go;
+}
+code:back() {
+  code ~/www/github/trackco/a2/frontend/track;
+}
 
-cxm:back() cd ~/www/github/trackco/a2/backend/cmd/core;
-code:cxm:back() code ~/www/github/trackco/a2/frontend/track;
+cd:front() {
+  cd ~/www/github/trackco/a2/frontend/track && pwd;
+}
+run:front() {
+  clear && cd:front && npm run dev;
+}
+code:front() {
+  code ~/www/github/trackco/a2/frontend/track;
+}
 
-cxm:front() cd ~/www/github/trackco/a2/frontend/track;
-code:cxm:front() code ~/www/github/trackco/a2/frontend/track;
+cd:ds() {
+  cd ~/www/github/trackco/design-system && pwd;
+}
+run:ds() {
+  clear && cd:ds && npm run storybook;
+}
+code:ds() {
+  code ~/www/github/trackco/design-system;
+}
 
-cxm:credits() cd ~/www/github/trackco/api-credits;
-code:cxm:credits() code ~/www/github/trackco/api-credits;
+# cd:credits() cd ~/www/github/trackco/api-credits && pwd;
+# code:credits() code ~/www/github/trackco/api-credits;
